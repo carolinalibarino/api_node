56b184960103a9e65962548152d3febab520d6ef
carolinalibarino <carollibarino2@gmail.com>
2025-08-19 09:05:23 -0300

    get e post

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from 'express'
 
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
+
 const app = express()
 
 app.use(express.json())
@@ -7,16 +11,24 @@ app.use(express.json())
 const usuarios = []
 
 //criar as rotas da API
-app.get('/cadastro', (req, res) => {
+app.get('/cadastro', async (req, res) => {
     //res.send('FUNFOU O GET!!!!!!')
     //res.json(usuarios)
-    res.status(200).json(usuarios)
+    const lista_usuarios = await prisma.usuario.findMany()
+    res.status(200).json(lista_usuarios)
 })
 
-app.post('/cadastro', (req, res) => {
+app.post('/cadastro', async (req, res) => {
     //console.log(req.body)
-    usuarios.push(req.body)
+    //usuarios.push(req.body)
     //res.send('FUNFOU O POST!!!!!!')
+    await prisma.usuario.create({
+        data: {
+            nome: req.body.nome,
+            email: req.body.email,
+            idade: req.body.idade
+        }
+    })
     res.status(201).json(req.body)
 })
 
